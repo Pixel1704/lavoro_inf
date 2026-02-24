97319264930978bea5c50306fae113e4daa7d5a3
edostano <73550844+Edos08XD@users.noreply.github.com>
2026-02-24 10:04:09 +0100

    uwyu

--- CandelaArtigianale.java
+++ CandelaArtigianale.java
@@ -1,3 +1,20 @@
 public class CandelaArtigianale {
-    
+
+    String tipoCera;
+    double pesoCera, tempoProduzione;
+    int numeroStampi;
+
+    public CandelaArtigianale(String tipoCera, double pesoCera, double tempoProduzione, int numeroStampi) {
+        this.tipoCera = tipoCera;
+        this.pesoCera = pesoCera;
+        this.tempoProduzione = tempoProduzione;
+        this.numeroStampi = numeroStampi;
+    }
+
+    public void descrizioneProduzione() {
+       System.out.println("Tipo cera: " + tipoCera);
+       System.out.println("Peso cera (g): " + pesoCera);
+       System.out.println("Tempo produzione (min): " + tempoProduzione);
+       System.out.println("Numero stampi: " + numeroStampi);
+   }
 }
