1a25f53c1072ac5dff07a2be03d435ef9f6219f4
edostano <73550844+Edos08XD@users.noreply.github.com>
2026-02-24 10:28:05 +0100

    aggiustato candelaindutriale

--- CandelaIndustriale.java
+++ CandelaIndustriale.java
@@ -3,14 +3,15 @@ public class CandelaIndustriale extends ProduzioneCandela {
     protected int livelloAutomazione; 
     
     public CandelaIndustriale(String tipoCera, double pesoCera, double tempoProduzione, int livelloAutomazione) {
-        // inizializzo campi protetti senza invocare super
-        this.tipoCera = tipoCera;
-        this.pesoCera = pesoCera;
-        this.tempoProduzione = tempoProduzione;
+        super(tipoCera, pesoCera, tempoProduzione);
         this.livelloAutomazione = livelloAutomazione;
     }
     
     public double calcolaEfficienza() {
         return (pesoCera / tempoProduzione) * livelloAutomazione;
     }
+
+    public void descrizioneProduzione() {
+       super.descrizioneProduzione();
+       System.out.println("Livello automazione: " + livelloAutomazione);
 }
